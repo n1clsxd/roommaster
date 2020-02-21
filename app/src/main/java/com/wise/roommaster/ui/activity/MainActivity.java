5dd9ff64a831d479d96f2f089bb997474eed4da1
Nicolas <nicolasmickus@gmail.com>
2020-02-21 17:09:26 -0300

    .

--- app/src/main/java/com/wise/roommaster/ui/activity/MainActivity.java
+++ app/src/main/java/com/wise/roommaster/ui/activity/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.wise.roommaster.R;
@@ -56,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
         String emailLogged = "";
         try{
             emailLogged = pref.getString("userEmail", null);
@@ -78,12 +81,7 @@ public class MainActivity extends AppCompatActivity {
             logoutTesteBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    editor.remove("userEmail");
-                    editor.remove("userName");
-                    editor.remove("companyId");
-                    editor.commit();
-                    System.out.println(pref.getString("userEmail",null));
-                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    quickLogout(editor, pref);
                 }
             });
             final Button roomTesteBtn = findViewById(R.id.room_teste);
@@ -102,19 +100,8 @@ public class MainActivity extends AppCompatActivity {
             quickMeetBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    quickMeet();
 
-                    try {
-                        String result = new CreateMeetingService(
-                                10,
-                                Globals.userId,//pref.getInt("userId",-1),
-                                "reuniao teste",
-                                new Date(1634464800),
-                                new Date(1634500800)
-
-                        ).execute().get();
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
                 }
             });
             final FloatingActionButton newMeetingBtn = findViewById(R.id.create_meeting_button);
@@ -132,11 +119,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
         super.onBackPressed();
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.main_activity_menu_test_logout){
+            final SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+            final SharedPreferences.Editor editor = pref.edit();
+            quickLogout(editor, pref);
+        }
+        if(item.getItemId() == R.id.main_activity_menu_test_allrooms){
+            setContentView(R.layout.activity_create_meeting);
+            findViewById(R.id.create_meeting_main).setVisibility(View.INVISIBLE);
+            findViewById(R.id.create_meeting_calendar_view).setVisibility(View.INVISIBLE);
+            findViewById(R.id.create_meeting_room_list).setVisibility(View.VISIBLE);
+            configRoomList();
+        }
+
+        if(item.getItemId() == R.id.main_activity_menu_test_quickmeet){
+            quickMeet();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void configMeetList() {
         System.out.println("iniciando config");
@@ -170,12 +190,38 @@ public class MainActivity extends AppCompatActivity {
         ListView roomList = findViewById(R.id.list_room_listview);
         RoomDAO roomDAO = new RoomDAO();
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-        roomDAO.updateRoomList(pref.getInt("companyId",0));
+        roomDAO.updateRoomList(Globals.companyId);
         List<Room> rooms = new RoomDAO().roomList();
 
 
         roomList.setAdapter(new RoomListAdapter(rooms, this));
 
+    }
+    private void quickMeet(){
+        try {
+            new CreateMeetingService(
+                    10,
+                    Globals.userId,//pref.getInt("userId",-1),
+                    "reuniao teste",
+                    new Date(1634464800),
+                    new Date(1634500800)
+
+            ).execute().get();
+            configMeetList();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void quickLogout(SharedPreferences.Editor editor, SharedPreferences pref) {
+        editor.remove("userEmail");
+        editor.remove("userName");
+        editor.remove("companyId");
+        editor.commit();
+        Globals.companyId = 0;
+        Globals.userId = 0;
+        System.out.println(pref.getString("userEmail",null));
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 
 }
